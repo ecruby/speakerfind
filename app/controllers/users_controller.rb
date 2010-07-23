a48f279867b7d546baf11f8d2820cc511688b3eb
Tony Eichelberger <tony.eich@gmail.com>
2010-07-23 06:29:18 -0500

    cleaning up some things for checkin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
@@ -7,12 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       redirect_to activation_url(@user)
-    end
-    
-  end
-
-  def show
-    
+    end    
   end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,12 +2,6 @@ require 'spec_helper'
 
 describe UsersController do
 
-  #Delete these examples and add some real ones
-  it "should use UsersController" do
-    controller.should be_an_instance_of(UsersController)
-  end
-
-
   describe "GET 'new'" do
     it "should create an instance of a user object" do
       get 'new'
@@ -28,9 +22,7 @@ describe UsersController do
                          }
                          
         response.should redirect_to activation_url(assigns(:user).id)
-        
       end
-      
       
     end
   end
